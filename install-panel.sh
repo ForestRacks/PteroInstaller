45760cc14f1250c1d3d319503bea9367f0945b43
Zinidia <zinidiapayments@gmail.com>
2021-12-26 17:05:35 -0600

    fixed printing on new line

--- install-panel.sh
+++ install-panel.sh
@@ -913,11 +913,11 @@ function main {
 
   # set FQDN
   while [ -z "$FQDN" ]; do
-      echo -n "* Point an A-Record for your panel subdomain to your machine IP on DNS."
-      echo -n "* Now set the FQDN (panel URL) eg. panel.forestracks.com: "
-      read -r FQDN
+    echo "* Point an A-Record for your panel subdomain to your machine IP on DNS."
+    echo "* "* Now set the FQDN (panel URL) eg. panel.forestracks.com: "
+    read -r FQDN
 
-      [ -z "$FQDN" ] && print_error "FQDN cannot be empty"
+    [ -z "$FQDN" ] && print_error "FQDN cannot be empty"
   done
 
   # UFW is available for Ubuntu/Debian
