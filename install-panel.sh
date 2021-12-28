ccfb65d5cc37ea5482d63e17bbd509e47027acd2
Zinidia <33679848+Zinidia@users.noreply.github.com>
2021-12-28 15:15:30 -0600

    Update install-panel.sh

--- install-panel.sh
+++ install-panel.sh
@@ -988,7 +988,7 @@ function main {
     print_error "Installation aborted."
     exit 1
   fi
-  mkdir /etc/pterodactyl
+  mkdir -p /etc/pterodactyl
   cp /var/www/pterodactyl/.env /etc/pterodactyl
 }
 
