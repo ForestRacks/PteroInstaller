fb554a4e9a5c2276385586d47e2672c9e6216992
Matthew Jacob <33679848+Zinidia@users.noreply.github.com>
2023-01-01 13:09:18 -0600

    Switched to Ubuntu 22.04 compatible redis repository

--- install-panel.sh
+++ install-panel.sh
@@ -418,7 +418,8 @@ function ubuntu_dep {
   
   # Add additional repositories for PHP, Redis, and MariaDB
   LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-  add-apt-repository -y ppa:chris-lea/redis-server
+  curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
   curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 
   # Update repositories list
